cc841c073c4fc61773a87c02bbb27786875a88f0
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-03 22:58:57 -0300

    conta bancaria simples

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     
+        //TODO:conhecer e importar a classe Scanner
+        
+        Scanner teclado = new Scanner(System.in);
+        //Exibir as mensagens para o nosso usuário
+        int numeroConta;
+        String agencia, nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor digita o numedo da conta: ");
+        numeroConta = teclado.nextInt();
+
+        System.out.println("Digite o numero da agencia: ");
+        agencia = teclado.next();
+
+        System.out.println("Digite seu nome: ");
+        nomeCliente = teclado.nextLine();
+
+        System.out.println("Digite seu saldo: ");
+        saldo = teclado.nextFloat();
+        //Obter pela classe scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+
+        System.out.println("Ola "+ nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numeroConta+" e seu saldo "+saldo+" já está disponível para saque");
+
     }
 }
